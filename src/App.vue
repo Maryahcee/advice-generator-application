fb20a5562f414491ba7d2048c0c913ffa788c5cd
MaryahKim <maryapitaz@gmail.com>
2022-03-21 21:37:31 +0300

    Update App.vue
    footer centering on small screen

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,8 @@
     </div>
   </div>
   <footer class="bg-emerald-100 p-5 fixed inset-x-0
-             bottom-0">
-      <div class="flex justify-center items-center text-sm">
+             bottom-0 sm:text-xs sm:p-2 sm:font-thin sm:tracking-tighter">
+      <div class=" text-sm">
         Challenge by <a class="text-rose-700" href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
         Coded by <a href="#" >  <span class="text-rose-700"> Mary Njoroge </span></a>.
       </div>
