6f959ecac4a9833dbb28d64f723896c0d324137f
MaryahKim <maryapitaz@gmail.com>
2022-03-21 19:06:43 +0300

    Update Heading.vue
    add header color
    Delete styles boiler-plate

--- src/components/Heading.vue
+++ src/components/Heading.vue
@@ -1,6 +1,6 @@
 <template>
   <div >
-      <h1 class="header text-green-400 p-4 pb-2 text-center text-xl text-[20px] tracking-widest">
+      <h1 class="header text-darker-green p-4 pb-2 text-center text-xl text-[20px] tracking-widest">
          {{ advice.heading}}  #{{ advice.id}}
       </h1>
       </div>
@@ -14,8 +14,3 @@ props: ['advice'],
 
 </script>
 
-<style>
-.header{
-    color: hsl(151, 100%, 66%);
-}
-</style>
