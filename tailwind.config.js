6edfc6f1f9f2843b526d3df0a84c5e1bae8a0892
MaryahKim <maryapitaz@gmail.com>
2022-03-21 19:07:44 +0300

    Update tailwind.config.js
    Add darker-green color to tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ const { slotFlagsText } = require("@vue/shared");
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'darker-green': '#52ffab',
+      }
+    },
     container: {
       center: true,
     },
